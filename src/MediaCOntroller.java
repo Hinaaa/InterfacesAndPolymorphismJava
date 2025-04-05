fb1220f909e4b8cda63937729d8778a474b2a697
hinajawaid <hinajawaid.h@gmail.com>
2025-04-05 23:05:30 +0200

    MediaController

---
+++ src/MediaCOntroller.java
@@ -0,0 +1,5 @@
+public class MediaCOntroller {
+    public void playMedia(Playable mediaObjects) {
+        mediaObjects.play();
+    }
+}
---
+++ src/VideoPlayer.java
@@ -0,0 +1,7 @@
+public class VideoPlayer implements Playable{
+
+    @Override
+    public void play() {
+        System.out.println("Music is being played with Video in Video Playerr");
+    }
+}
